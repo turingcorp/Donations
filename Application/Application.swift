f49abc3543706f7f64350ec55558b01f417697e7
vauxhall <zero.griffin@gmail.com>
2018-11-05 22:12:01 +0100

    Configure project initial settings

---
+++ Application/Application.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+@UIApplicationMain class Application:UIResponder, UIApplicationDelegate {
+    var window:UIWindow?
+    
+    func application(_:UIApplication, didFinishLaunchingWithOptions:[UIApplication.LaunchOptionsKey:Any]?) -> Bool {
+        window = UIWindow(frame:UIScreen.main.bounds)
+        window!.backgroundColor = .black
+        window!.makeKeyAndVisible()
+        window!.rootViewController = UIViewController()
+        return true
+    }
+}
