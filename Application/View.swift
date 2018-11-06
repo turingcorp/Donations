ab41c4d8233466828266eb198d71fb58b088bb4a
vauxhall <zero.griffin@gmail.com>
2018-11-06 01:55:36 +0100

    Add collection view

--- Application/View.swift
+++ Application/View.swift
@@ -1,8 +1,9 @@
 import UIKit
 
-class View:UIViewController {
+class View:UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     private var items = [Item]()
     private let presenter = Presenter()
+    private weak var collection:UICollectionView!
     private weak var indicator:UIActivityIndicatorView!
     private weak var message:UILabel!
     private weak var refresh:UIButton!
@@ -14,6 +15,33 @@ class View:UIViewController {
             self?.update(viewModel:viewModel)
         }
         presenter.refresh()
+    }
+    
+    override func viewWillTransition(to size:CGSize, with coordinator:UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to:size, with:coordinator)
+        collection.collectionViewLayout.invalidateLayout()
+    }
+    
+    func collectionView(_:UICollectionView, numberOfItemsInSection:Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_:UICollectionView, layout:UICollectionViewLayout, sizeForItemAt:IndexPath) -> CGSize {
+        return collection.bounds.size
+    }
+    
+    func collectionView(_:UICollectionView, cellForItemAt index:IndexPath) -> UICollectionViewCell {
+        let cell = collection.dequeueReusableCell(withReuseIdentifier:"cell", for:index) as! Cell
+        cell.item = items[index.item]
+        return cell
+    }
+    
+    func collectionView(_:UICollectionView, shouldSelectItemAt:IndexPath) -> Bool {
+        return false
+    }
+    
+    func collectionView(_:UICollectionView, shouldHighlightItemAt:IndexPath) -> Bool {
+        return false
     }
     
     private func makeOutlets() {
@@ -45,6 +73,23 @@ class View:UIViewController {
         view.addSubview(refresh)
         self.refresh = refresh
         
+        let flow = UICollectionViewFlowLayout()
+        flow.sectionInset = UIEdgeInsets(top:20, left:20, bottom:20, right:20)
+        flow.minimumLineSpacing = 0
+        flow.minimumInteritemSpacing = 10
+        flow.scrollDirection = .horizontal
+        
+        let collection = UICollectionView(frame:.zero, collectionViewLayout:flow)
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.backgroundColor = .clear
+        collection.alwaysBounceHorizontal = true
+        collection.isPagingEnabled = true
+        collection.delegate = self
+        collection.dataSource = self
+        collection.register(Cell.self, forCellWithReuseIdentifier:"cell")
+        view.addSubview(collection)
+        self.collection = collection
+        
         indicator.centerXAnchor.constraint(equalTo:view.centerXAnchor).isActive = true
         indicator.centerYAnchor.constraint(equalTo:view.centerYAnchor).isActive = true
         
@@ -56,6 +101,17 @@ class View:UIViewController {
         refresh.centerXAnchor.constraint(equalTo:view.centerXAnchor).isActive = true
         refresh.widthAnchor.constraint(equalToConstant:130).isActive = true
         refresh.heightAnchor.constraint(equalToConstant:38).isActive = true
+        
+        collection.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
+        collection.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
+        
+        if #available(iOS 11.0, *) {
+            collection.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor).isActive = true
+            collection.bottomAnchor.constraint(equalTo:view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        } else {
+            collection.topAnchor.constraint(equalTo:view.topAnchor).isActive = true
+            collection.bottomAnchor.constraint(equalTo:view.bottomAnchor).isActive = true
+        }
     }
     
     private func update(viewModel:ViewModel) {
@@ -63,5 +119,6 @@ class View:UIViewController {
         refresh.isHidden = viewModel.refreshHidden
         indicator.isHidden = viewModel.indicatorHidden
         items = viewModel.items
+        collection.reloadData()
     }
 }
